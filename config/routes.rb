520d2b996323fc8855b1f888b82020d5030fee80
Jamie Vickrey <james_vickrey@yahoo.com>
2013-08-30 15:58:17 -0700

    added a contact resource and set the root page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ CfWeekThree::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +14,8 @@ CfWeekThree::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :contacts
+
 
   # Example resource route with options:
   #   resources :products do
